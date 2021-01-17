7228538e6d4c89c979459e87c4f3db95b0530023
Mohammad abukaff <q_k_86@hotmail.com>
2021-01-17 10:39:57 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <script src="jquery.min.js" type="text/javascript"></script>
+        <link href="bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <meta charset="UTF-8">
+        <style>
+            body{
+                background-image: url("img12.jpg");
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+            h2.x{
+                border-style: double;
+                border-top-width: thick;
+             
+                
+            }
+        </style>
+        <title></title>
+    </head>
+    <body>
+        <br><br><br><br>
+    <center>
+        <div>Hello my frnd</div>
+        <br><br><br><br>
+
+        <h1>Welcom to our project</h1>
+         <br><br><br><br>
+         <h2 class="x">Borders Style</h2>
+          <br><br><br><br>
+          <h2 class="y">My style </h2>
+    </center>
+
+</body>
+</html>
